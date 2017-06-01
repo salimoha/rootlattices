c00d72afe846ecdbb86ef158a7bdec5feea2109f
Shahrouz Alimo <salimoha@ucsd.edu>
2017-06-01 13:40:06 -0700

    Open software license

--- Unconstraint_quantizer.py
+++ Unconstraint_quantizer.py
@@ -2,6 +2,26 @@ import numpy as np
 import numpy.linalg as nplg
 import sys
 
+'''MIT License
+Copyright (c) Shahrouz Ryan Alimo 2017
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+Modified: Feb. 2017 '''
+
+
 # %% Restriction Algorithm Library
 # %
 # %
@@ -13,6 +33,8 @@ import sys
 # %    Domain setup.
 # %    This function will restrict a point x onto the lattice defined by the variable 'lattice'
 # %defined by the coarseness variable 'scale = 1/MeshSize'.
+
+
 from init_DOGS import init_DOGS
 
 
